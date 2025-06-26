1a4e05c6a9aa5b3865e789a0f4e31570e5206b77
Hilary367 <2711381922@qq.com>
2025-06-26 21:55:34 +0800

    Update opiModel.vue

--- src/views/advertisingManagement/advertisingPlan/opiModel.vue
+++ src/views/advertisingManagement/advertisingPlan/opiModel.vue
@@ -1,97 +1,153 @@
 <template>
-    <div class="form">
-        <el-form :model="ruleForm" ref="ruleForm" label-width="120px" class="demo-ruleForm">
-            <el-form-item label="名称" prop="name" :rules="[{required: true, message: '不能为空', trigger: 'blur'}]">
-                <el-input v-model="ruleForm.name"></el-input>
-            </el-form-item>
-            <el-form-item label="广告类型" prop="type">
-                <el-select v-model="ruleForm.type" @change="selAdvertisingType" placeholder="广告类型">
-                    <el-option label="图片" value="IMAGE"></el-option>
-                    <el-option label="视频" value="VIDEO"></el-option>
-                </el-select>
-            </el-form-item>
-            <el-form-item v-if="isTime" label="间隔时间(秒)" prop="interval"
-                          :rules="[{required: true, message: '不能为空', trigger: 'blur'}]">
-                <el-input type="number" v-model="ruleForm.interval"></el-input>
-            </el-form-item>
-            <el-form-item label="设备类型" prop="deviceTypeId"
-            >
-                <el-select v-model="ruleForm.deviceTypeId" placeholder="设备类型" @change="selEquipmentType">
-                    <el-option v-for="item in equipmentType" :label="item.name" :value="item.id"></el-option>
-                </el-select>
-            </el-form-item>
-            <el-form-item label="广告位类型" prop="adSpaceTypeId"
-            >
-                <el-select v-model="ruleForm.adSpaceTypeId" placeholder="广告位类型">
-                    <el-option v-for="item in advertisingSpaceType" :label="item.name"
-                               :value="item.id"></el-option>
-                </el-select>
-            </el-form-item>
-            <el-form-item label="设备" prop="equipment">
-                <span style="cursor: pointer;" v-if="!ruleForm.deviceIds.length" @click="addEquipment=true">选择设备</span>
-                <span style="cursor: pointer;" v-else @click="addEquipment=true">{{ruleForm.deviceIds.length}}个设备</span>
-                <!--<div v-if="!ruleForm.equipment.length" class="el-form-item__error">-->
-                <!--不能为空-->
-                <!--</div>-->
-            </el-form-item>
-            <el-form-item label="素材" prop="resources">
-                <span v-if="!ruleForm.resources.length" style="cursor: pointer;" @click="clickMaterial">选择素材</span>
-                <span v-else style="cursor: pointer;" @click="clickMaterial">已选{{ruleForm.resources.length}}个素材</span>
-                <!--<div v-if="!ruleForm.sourceMaterial.length" class="el-form-item__error">-->
-                <!--不能为空-->
-                <!--</div>-->
-            </el-form-item>
-            <div class="selector" v-if="ruleForm.resources.length">
-                <p class="selectedMaterial" style="padding-left: 50px;">已选素材</p>
-                <div class="selector-check">
-                    <ul v-if="!isTime">
-                        <li v-for="(item,index) in ruleForm.resources" :key="index"
-                            @click="clickVideoUrl(item)" style="cursor: pointer;">
-                            <video :src="item"></video>
-                            <span class="bofang">
-                              <svg-icon icon-class="bofang"></svg-icon>
-                             </span>
-                        </li>
-                    </ul>
-                    <ul v-else>
-                        <li v-for="(item,index) in ruleForm.resources" :key="index">
-                            <img :src="item"/>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <el-form-item style="text-align: center;">
-                <el-button type="primary" @click="submitForm('ruleForm')">{{submitTitle}}</el-button>
-                <el-button @click="resetForm('ruleForm')">重置</el-button>
-            </el-form-item>
-        </el-form>
-
-
-        <el-dialog
-                width="300px"
-                :visible.sync="addEquipment">
-            <add-equipment ref="transfer" :deviceIds="ruleForm.deviceIds" v-if="addEquipment"></add-equipment>
-            <div slot="footer" class="dialog-footer">
-                <el-button @click="addEquipment = false">取消</el-button>
-                <el-button type="primary" @click="determine">确定</el-button>
-            </div>
-        </el-dialog>
-
-        <el-dialog
-                width="800px"
-                :visible.sync="addSourceMaterial">
-            <select-material ref="selectMaterial" :currType="type" :resources="ruleForm.resources" v-if="addSourceMaterial"></select-material>
-            <div slot="footer" class="dialog-footer">
-                <el-button @click="addSourceMaterial = false">取消</el-button>
-                <el-button type="primary" @click="determineSelectMaterial">确定</el-button>
-            </div>
-        </el-dialog>
-
-
-        <el-dialog :visible.sync="detialPlayVideo" :title="title" v-if="state" @close="close">
-            <player :video-url="videoUrl"></player>
-        </el-dialog>
-    </div>
+    <div>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column type="index" :index="indexMethod"></el-table-column>
+      <el-table-column label="NO" width="180">
+        <template #default="scope">
+          <el-table style="width: 100%">
+            <el-table-column label="属性" width="100">
+              <template #default>
+                <span>NO</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="值" width="100">
+              <template #default>
+                <span>{{ scope.row.number }}</span>
+              </template>
+            </el-table-column>
+          </el-table>
+        </template>
+      </el-table-column>
+      <el-table-column label="第一次加水量" width="180">
+        <template #default="scope">
+          <el-table style="width: 100%">
+            <el-table-column label="属性" width="100">
+              <template #default>
+                <span>第一次加水量</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="值" width="100">
+              <template #default>
+                <span>{{ scope.row.water_one }}</span>
+              </template>
+            </el-table-column>
+          </el-table>
+        </template>
+      </el-table-column>
+      <el-table-column label="浸泡时间">
+        <template #default="scope">
+          <el-table style="width: 100%">
+            <el-table-column label="属性" width="100">
+              <template #default>
+                <span>浸泡时间</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="值" width="100">
+              <template #default>
+                <span>{{ scope.row.jing_time }}</span>
+              </template>
+            </el-table-column>
+          </el-table>
+        </template>
+      </el-table-column>
+      <el-table-column label="武火时间">
+        <template #default="scope">
+          <el-table style="width: 100%">
+            <el-table-column label="属性" width="100">
+              <template #default>
+                <span>武火时间</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="值" width="100">
+              <template #default>
+                <span>{{ scope.row.wuhuo_time }}</span>
+              </template>
+            </el-table-column>
+          </el-table>
+        </template>
+      </el-table-column>
+      <el-table-column label="一煎时间">
+        <template #default="scope">
+          <el-table style="width: 100%">
+            <el-table-column label="属性" width="100">
+              <template #default>
+                <span>一煎时间</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="值" width="100">
+              <template #default>
+                <span>{{ scope.row.first_time }}</span>
+              </template>
+            </el-table-column>
+          </el-table>
+        </template>
+      </el-table-column>
+      <el-table-column label="第二次加水时间">
+        <template #default="scope">
+          <el-table style="width: 100%">
+            <el-table-column label="属性" width="100">
+              <template #default>
+                <span>第二次加水时间</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="值" width="100">
+              <template #default>
+                <span>{{ scope.row.water_two}}</span>
+              </template>
+            </el-table-column>
+          </el-table>
+        </template>
+      </el-table-column>
+      <el-table-column label="二煎时间">
+        <template #default="scope">
+          <el-table style="width: 100%">
+            <el-table-column label="属性" width="100">
+              <template #default>
+                <span>二煎时间</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="值" width="100">
+              <template #default>
+                <span>{{ scope.row.second_time }}</span>
+              </template>
+            </el-table-column>
+          </el-table>
+        </template>
+      </el-table-column>
+      <el-table-column label="吸水量">
+        <template #default="scope">
+          <el-table style="width: 100%">
+            <el-table-column label="属性" width="100">
+              <template #default>
+                <span>吸水量</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="值" width="100">
+              <template #default>
+                <span>{{ scope.row.water_account }}</span>
+              </template>
+            </el-table-column>
+          </el-table>
+        </template>
+      </el-table-column>
+      <el-table-column label="蒸发量">
+        <template #default="scope">
+          <el-table style="width: 100%">
+            <el-table-column label="属性" width="100">
+              <template #default>
+                <span>蒸发量</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="值" width="100">
+              <template #default>
+                <span>{{ scope.row.evaporation }}</span>
+              </template>
+            </el-table-column>
+          </el-table>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 <script>
     import {EQUIPMENTTYPE, ADVERTISINGSPACETYPE, POST_LIST, DETAIL_LIST, PUT_LIST} from '@/api/advertisingManagement'
@@ -103,194 +159,32 @@
     export default {
         data() {
             return {
-                submitTitle:'立即创建',
-                detialPlayVideo: false,
-                state: false,
-                videoUrl: '',
-                addEquipment: false,
-                addSourceMaterial: false,
-                isTime: true,
-                ruleForm: {
-                    name: '',
-                    type: 'IMAGE',
-                    interval: '',
-                    deviceTypeId: '',
-                    adSpaceTypeId: '',
-                    deviceIds: [],
-                    resources: []
-                },
-                equipmentType: '',
-                advertisingSpaceType: '',
-                equipment: '',
-                type: '',
-                deviceTypeId: ''
+                tableData: [
+                    {
+                        number: "",
+                        water_one: "",
+                        jing_time: "",
+                        wuhuo_time:"",
+                        first_time:"",
+                        water_two:"",
+                        second_time:"",
+                        water_account:"",
+                        evaporation:""
+
+
+                    },
+                ]
             };
         },
         components: {
-            addEquipment,
-            selectMaterial,
-            player
+            
         },
         mounted() {
-            this.getEquipmentType();
-            this.DETAIL_LIST();
+            
 
         },
         methods: {
-            ...mapActions(["delVisitedView"]),
-            /**
-             * 确定
-             */
-            submitForm(formName) {
-                let that = this;
-                let id = this.$route.query.id;
-                this.$refs[formName].validate((valid) => {
-                    if (valid) {
-                        if (id) {
-                            this.ruleForm.id = id;
-                            PUT_LIST(this.ruleForm).then(res => {
-                                Alert.success('更新成功');
-                                setTimeout(() => {
-                                    that.delVisitedView(that.$route).then(res => {
-                                        that.$router.go(-1);
-                                    });
-                                }, 500)
-                            });
-                        } else {
-                            POST_LIST(this.ruleForm).then(res => {
-                                Alert.success('添加成功');
-                                setTimeout(() => {
-                                    that.delVisitedView(that.$route).then(res => {
-                                        that.$router.go(-1);
-                                    });
-                                }, 500)
-                            });
-                        }
 
-                    }
-                });
-
-
-            },
-
-            DETAIL_LIST() {
-                let id = this.$route.query.id;
-                if (id == undefined) {
-                    return
-                }
-                this.submitTitle = '立即更新';
-                let params = {
-                    id
-                };
-                DETAIL_LIST(params).then(res => {
-                    this.ruleForm = res;
-                    this.isTime = res.type != 'VIDEO';
-                })
-            },
-
-            /**
-             * 重置
-             */
-
-            resetForm(formName) {
-                this.$refs[formName].resetFields();
-            },
-
-            /**
-             * 获取广告位类型
-             */
-            getAdvertisingSpaceType() {
-                let params = {
-                    deviceTypeId: this.deviceTypeId
-                };
-                ADVERTISINGSPACETYPE(params).then(res => {
-                    console.log(res);
-                    this.advertisingSpaceType = res
-                });
-            },
-
-            /**
-             *  获取设备
-             */
-            getEquipmentType() {
-                EQUIPMENTTYPE().then(res => {
-                    res.unshift({
-                        id: '',
-                        name: '请选择'
-                    });
-                    this.equipmentType = res;
-                });
-            },
-
-            /**
-             * 广告类型 change
-             * @param e
-             */
-            selAdvertisingType(e) {
-                if (e == 'IMAGE') {
-                    this.isTime = true;
-                } else {
-                    this.isTime = false;
-                }
-
-                this.ruleForm = {
-                    ...this.ruleForm,
-                    resources: []
-                };
-            },
-            selEquipmentType(e) {
-                this.deviceTypeId = e;
-                this.getAdvertisingSpaceType()
-            },
-            determine() {
-                let childList = [];
-                this.$refs.transfer.list.map((item) => {
-                    if (item.checked) {
-                        childList.push(item.id)
-                    }
-                    return item
-                });
-                this.addEquipment = false;
-                this.ruleForm = {
-                    ...this.ruleForm,
-                    deviceIds: childList
-                }
-
-            },
-
-            clickMaterial() {
-                this.addSourceMaterial = true;
-                this.type = this.ruleForm.type
-            },
-
-            /**
-             * 选择素材
-             */
-            determineSelectMaterial() {
-                let childList = [];
-                this.$refs.selectMaterial.lists.map((item) => {
-                    if (item.checked) {
-                        childList.push(item.url)
-                    }
-                    return item
-                });
-                this.addSourceMaterial = false;
-                this.ruleForm = {
-                    ...this.ruleForm,
-                    resources: childList
-                };
-            },
-
-            clickVideoUrl(src, name) {
-                this.detialPlayVideo = true;
-                this.state = true;
-                this.videoUrl = src;
-                this.title = name
-            },
-
-            close() {
-                this.state = false
-            }
         }
     }
 </script>
