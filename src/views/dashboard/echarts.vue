f50e430ed8e6c1933a7486c3905e9f5960ce9e26
Hilary367 <2711381922@qq.com>
2025-06-30 23:27:59 +0800

    2

---
+++ src/views/dashboard/echarts.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+<script>
+
+export default {
+  data() {
+    return {
+      
+    };
+  },
+  components: {
+    
+  },
+  async mounted() {
+    
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="pageContainer">
-    <Row1/>
+    <!-- <Row1/>
     <Row2/>
-    <Row3></Row3>
+    <Row3></Row3> -->
+    <Echarts></Echarts>
   </div>
 </template>
 
@@ -11,6 +12,7 @@ const Row2Tab = () => import("./row2Tab/row2Tab");
 const Row3 = () => import("./row3/index");
 const Row2 = () => import("./row2/index");
 const Row1 = () => import("./row1");
+const Echarts = () => import("./echarts")
 export default {
   name: "index",
   data() {
@@ -18,10 +20,11 @@ export default {
   },
   methods: {},
   components: {
-    Row1,
-    Row2,
-    // Row2Tab,
-    Row3
+    Echarts
+    // Row1,
+    // Row2,
+    // // Row2Tab,
+    // Row3
   }
 };
 </script>
